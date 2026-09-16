1b9e81c9325f4f773cdf88e54c1ffe8d3858da05
Sai Santosh Reddy <saisantoshreddy999@gmail.com>
2026-09-16 09:54:02 +0530

    Backend foundation and User CRUD

---
+++ Backend/main.py
@@ -0,0 +1,70 @@
+from fastapi import FastAPI,Depends
+from database import engine, Base,get_db
+from sqlalchemy.orm import Session
+from schemas import UserCreate
+from models import User
+Base.metadata.create_all(bind=engine)
+app = FastAPI(title="CareerLink")
+
+@app.get("/")
+def home():
+    return {"message": "CareerLink API is running"}
+
+@app.get("/test-db")
+def test_database():
+    try:
+        with engine.connect() as connection:
+            return {"message": "Database connected successfully"}
+    except Exception as e:
+        return {"error": str(e)}
+    
+@app.post("/users")
+def create_user(user: UserCreate, db: Session = Depends(get_db)):
+    new_user = User(
+        name=user.name,
+        email=user.email,
+        password=user.password,
+        role=user.role
+    )
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+@app.get("/users")
+def get_users(db: Session = Depends(get_db)):
+    users = db.query(User).all()
+    return users
+
+@app.get("/users/{user_id}")
+def get_user(user_id: int, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.id == user_id).first()
+    if not user:
+        return {"message": "User not found"}
+    return user
+
+@app.put("/users/{user_id}")
+def update_user(
+    user_id: int,
+    user: UserCreate,
+    db: Session = Depends(get_db)
+):
+    existing_user = db.query(User).filter(User.id == user_id).first()
+    if not existing_user:
+        return {"message": "User not found"}
+    existing_user.name = user.name
+    existing_user.email = user.email
+    existing_user.password = user.password
+    existing_user.role = user.role
+    db.commit()
+    db.refresh(existing_user)
+    return existing_user
+
+@app.delete("/users/{user_id}")
+def delete_user(user_id: int, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.id == user_id).first()
+    if not user:
+        return {"message": "User not found"}
+    db.delete(user)
+    db.commit()
+    return {"message": "User deleted successfully"}
---
+++ Backend/progress.md
@@ -0,0 +1,35 @@
+# CareerLink — Project Progress
+
+## Day 1 — Backend Foundation ✅
+
+### **Completed**
+- FastAPI backend setup
+- MySQL + SQLAlchemy configuration
+- Environment variables with `.env`
+- User model and schema
+- User CRUD APIs
+- Swagger API testing
+
+### **APIs**
+- `POST /users` — Create user
+- `GET /users` — Get all users
+- `GET /users/{id}` — Get user
+- `PUT /users/{id}` — Update user
+- `DELETE /users/{id}` — Delete user
+
+### **Tech Used**
+- Python
+- FastAPI
+- SQLAlchemy
+- MySQL
+- Pydantic
+
+### **Project Structure**
+
+```text
+CareerLink/
+├── Backend/
+├── Frontend/
+├── README.md
+├── PROGRESS.md
+└── .gitignore
